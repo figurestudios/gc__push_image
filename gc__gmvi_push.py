2e425d58657ca8abf3fa42efd58ddf55a11dc2d6
figurestudios <64747030+figurestudios@users.noreply.github.com>
2022-06-19 13:27:29 +0200

    add token

--- gc__gmvi_push.py
+++ gc__gmvi_push.py
@@ -5,19 +5,24 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('-f', '--file', type=str)
 parser.add_argument('-p', '--portal', type=str, default="https://siasky.net")
+parser.add_argument('-t', '--token', type=str) # you need to obtain this manually from a private/sign-up portal
+
+# parser.add_argument('-s', '--sponsor', type=str)
+# not sure if sponsored links are possible to get direct links for
 
 args = parser.parse_args()
 
 print("portal", args.portal)
 print("file", args.file)
+print("token", args.token)
 
-# public portals
+# public portals, required unless a token is specified
 portals = ["http://siasky.net:80/",
            "http://web3portal.com:80/"]
 
 print("initializing client")
 
-client = skynet.SkynetClient(args.portal)
+client = skynet.SkynetClient(args.portal, {"skynet_api_key": args.token})
 
 print("uploading file")
 
